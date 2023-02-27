057edf4bca09143d112208f425f05f5e6e490733
JoaoRuani <jvitornegri@gmail.com>
2023-02-27 20:29:20 -0300

    Pizza topping component initial

---
+++ src/components/PizzaTopping/PizzaTopping.js
@@ -0,0 +1,17 @@
+import './PizzaTopping.css';
+
+export default function PizzaTopping({ image, toppingName, toppingDescription }) {
+    return (
+        <div className="pizza-topping-card">
+            <img
+                src={image}
+                alt={toppingName}
+                className="pizza-topping-img"
+            />
+            <div className='pizza-topping-text'>
+                <h3 className='pizza-topping-name'>{toppingName}</h3>
+                <p className='pizza-topping-description'>{toppingDescription}</p>
+            </div>
+        </div>
+    );
+}
